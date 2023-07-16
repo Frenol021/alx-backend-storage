da74072d02bce0ab1306c09924dc6fe2ecffbe1e
frenol021 <frenol021@gmail.com>
2023-07-16 14:58:20 +0000

    creating tables

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,5 @@
+-- A SQL script that creates a table (called users) following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
 CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
